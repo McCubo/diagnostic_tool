f48aa113657084ba26848220559aee8b7389ce18
Miguel Cubias <m.caceres@polsource.com>
2021-03-09 14:31:35 -0600

    adding new custom type to datatable

--- force-app/main/default/lwc/vdt_datatable/vdt_datatable.js
+++ force-app/main/default/lwc/vdt_datatable/vdt_datatable.js
@@ -1,12 +1,17 @@
 import LightningDatatable from 'lightning/datatable';
 import vdt_progressBar from './vdt_progressBar';
+import vdt_enrichedText from './vdt_enrichedText';
 
 export default class Vdt_datatable extends LightningDatatable  {
 
     static customTypes = {
         vdt_progressBar: {
             template: vdt_progressBar,
-            typeAttributes: ['recordId', 'actualValue', 'targetValue']
+            typeAttributes: ['recordId', 'actualValue', 'targetValue', 'type']
+        },
+        vdt_enrichedText: {
+            template: vdt_enrichedText,
+            typeAttributes: ['recordId', 'textValue', 'helpMessage', 'formulaMessage']
         }
     }
 
