5b9b53c8bb1308337894e8529f2a46634351d53a
Miguel Cubias <m.caceres@polsource.com>
2021-03-17 18:08:49 -0600

    detail data calculations completed

--- force-app/main/default/lwc/vdt_onekeyData/vdt_onekeyData.js
+++ force-app/main/default/lwc/vdt_onekeyData/vdt_onekeyData.js
@@ -1,10 +1,15 @@
 import { LightningElement, track } from 'lwc';
 import searchExistingCalculations from '@salesforce/apex/VDT_MasterDataAnalysisController.searchExistingCalculations';
+import recalculateMasterAnalysis from '@salesforce/apex/VDT_MasterDataAnalysisController.recalculateMasterAnalysis';
+import validateCanRunCalculation from '@salesforce/apex/VDT_ObjectsCalculationController.validateCanRunCalculation';
+
+import { showToast } from 'c/vdt_utils';
 export default class Vdt_onekeyData extends LightningElement {
     
     _showCalculationButton = false;
     _showCalculationSection = false;
     _filterDisabled = false;
+    _calculationInProgress = false;
     _filter;
     countries = [];
 
@@ -23,8 +28,7 @@ export default class Vdt_onekeyData extends LightningElement {
         return `Calculations are not available for ${this._filter.countries} for the selected time period.`;
     }
 
-    handleShowInfo(event) {
-        this._showCalculationButton = true;
+    handleShowInfo(event) {        
         this._showCalculationSection = false;
         this._filterDisabled = true;
         this._filter = event.detail;
@@ -32,11 +36,34 @@ export default class Vdt_onekeyData extends LightningElement {
         searchExistingCalculations({ jsonSearchParameters: JSON.stringify(this._filter) }).then(response => {
             this._calculation = response;
             this._showCalculationSection = true;
+            this._showCalculationButton = true;
         }).catch(error => {
             console.log('Error in promise: %O', error);
         }).finally(() => {
             this._filterDisabled = false;
         });
+    }
+
+    handleCalculate(event) {
+        this._calculationInProgress = true;
+        validateCanRunCalculation()
+        .then((response) => {
+            if (response) {
+                recalculateMasterAnalysis({jsonSearchParameters: JSON.stringify(this._filter)})
+                .then(() => {
+                    this._calculation.status = 'In Progress';
+                })
+                .catch(error => {
+                     console.log(error);
+                })
+            } else {
+                this.dispatchEvent(showToast('Limit of calculation requests has been reached', 'warning'));
+            }
+        })
+        .catch((error) => {
+            this.dispatchEvent(showToast(error, 'error'));
+        })
+        .finally(() => this._calculationInProgress = false);;
     }
 
 }
