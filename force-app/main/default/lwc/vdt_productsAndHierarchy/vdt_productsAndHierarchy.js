722bb49298c62aa2d8de21e87d406dbc66f14790
Miguel Cubias <m.caceres@polsource.com>
2021-03-29 14:02:46 -0600

    using publish/subscribe methods to update table based on country selection

--- force-app/main/default/lwc/vdt_productsAndHierarchy/vdt_productsAndHierarchy.js
+++ force-app/main/default/lwc/vdt_productsAndHierarchy/vdt_productsAndHierarchy.js
@@ -1,57 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import onekeyCountryChannel from '@salesforce/messageChannel/vdt_onekeyCountryChannel__c';
 export default class Vdt_productsAndHierarchy extends LightningElement {
     
-    
+    @wire(MessageContext)
+    messageContext;
 
-    _countries = [
+    _countryOptions = [
         {label: 'GB', value: 'GB', selected: false},
         {label: 'FR', value: 'FR', selected: false},
         {label: 'US', value: 'US', selected: false},
         {label: 'DE', value: 'DE', selected: false}
     ];
-    
-    search = {
-        types: [],
-        countries: []
-    };
+
+    _countries = [];
 
     handleCountryChange(event) {
-        this.search.countries = event.detail;
-        this.treeData = this.filterData(JSON.parse(JSON.stringify(this.items)));
-    }
-    handleProductTypeChange(event) {
-        this.search.types = event.detail;
-        this.treeData = this.filterData(JSON.parse(JSON.stringify(this.items)));
-        // this.data = this.filterTableData(JSON.parse(JSON.stringify(ADOPTION_DATA)));
-    }
-    filterData(items) {
-        let filtered = items.filter((recordRow) => {
-            if (recordRow.items && recordRow.items.length > 0) {
-                recordRow.items = this.filterData(recordRow.items);
-            }
-            if (this.search.types.length > 0 && this.search.countries.length > 0) {
-                return this.search.types.includes(recordRow.type) && this.search.countries.includes(recordRow.country);
-            } else if (this.search.types.length == 0 && this.search.countries.length == 0) {
-                return true;
-            } else if (this.search.types.length > 0) {
-                return this.search.types.includes(recordRow.type)
-            } else if (this.search.countries.length > 0) {
-                return this.search.countries.includes(recordRow.country);
-            } else {
-                return false;
-            }
-        });
-        return filtered;
+        const payload = { countries: event.detail };
+        publish(this.messageContext, onekeyCountryChannel, payload);
+        this._countries = event.detail;
     }
 
-    filterTableData(records) {
-        let filtered = records.filter((recordRow) => {
-            return this.search.types.includes(recordRow.type);
-        });
-        return filtered;
-    }
-
-    handleDateRangeChange(evt) {
-        console.log(JSON.stringify(evt.detail));
-    }
 }
