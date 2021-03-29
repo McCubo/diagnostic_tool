f1d822f81b303211b341092504063029d7c4a5d5
Miguel Cubias <m.caceres@polsource.com>
2021-03-29 16:15:03 -0600

    enhanced tree hierarchy method

--- force-app/main/default/lwc/vdt_productAdoptionTree/vdt_productAdoptionTree.js
+++ force-app/main/default/lwc/vdt_productAdoptionTree/vdt_productAdoptionTree.js
@@ -1,150 +1,5 @@
 import { LightningElement } from 'lwc';
-
-const PRODUCT_DATA = [
-    {
-        label: 'Oncology',
-        name: '1',
-        expanded: true,
-        type: 'Detail',
-        country: 'GB',
-        metatext: 'Product Type: Detail | Country: GB',
-        items: [
-            {
-                label: 'Product Onco',
-                name: '2',
-                expanded: true,
-                type: 'Detail',
-                country: 'GB',
-                metatext: 'Product Type: Detail | Country: GB',
-                items: [
-                    {
-                        label: 'Promotional ONCO #1',
-                        name: '3',
-                        expanded: true,
-                        type: 'High Value Promotional',
-                        country: 'GB',
-                        metatext: 'Product Type: High Value Promotional | Country: GB',
-                        items: [],
-                    },
-                    {
-                        label: 'Promotional ONCO #2',
-                        name: '4',
-                        expanded: true,
-                        type: 'Promotional',
-                        country: 'GB',
-                        metatext: 'Product Type: Promotional | Country: GB',
-                        items: [],
-                    },
-                    {
-                        label: 'Promotional ONCO #3',
-                        name: '5',
-                        expanded: true,
-                        type: 'Promotional',
-                        country: 'GB',
-                        metatext: 'Product Type: Promotional | Country: GB',
-                        items: [],
-                    },                        
-                ],
-            },
-        ],
-    },
-    {
-        label: 'Paediatrics',
-        name: '6',
-        expanded: true,
-        type: 'Sample',
-        country: 'FR',
-        metatext: 'Product Type: Sample | Country: FR',
-        items: [
-            {
-                label: 'Product Pediatric',
-                name: '7',
-                expanded: true,
-                type: 'Sample',
-                country: 'FR',
-                metatext: 'Product Type: Sample | Country: FR',
-                items: [
-                    {
-                        label: 'Detail Topic Pediatrics #1',
-                        name: '8',
-                        expanded: true,
-                        type: 'Detail Topic',
-                        country: 'FR',
-                        metatext: 'Product Type: Detail Topic | Country: FR',
-                        items: [],
-                    }
-                ],
-            },
-        ],
-    },
-    {
-        label: 'Emergency Medicine',
-        name: '10',
-        type: 'Market',
-        country: 'DE',
-        metatext: 'Product Type: Market | Country: DE',
-        expanded: true,
-        items: [
-            {
-                label: 'Respiratory',
-                name: '11',
-                expanded: true,
-                type: 'Market',
-                country: 'DE',
-                metatext: 'Product Type: Market | Country: DE',
-                items: [
-                    {
-                        label: 'Ambulatory Equipment',
-                        name: '12',
-                        expanded: true,
-                        type: 'Market',
-                        country: 'DE',
-                        metatext: 'Product Type: Market | Country: DE',
-                        items: [],
-                    },
-                    {
-                        label: 'Tracheostomy',
-                        name: '13',
-                        expanded: true,
-                        type: 'Market',
-                        country: 'DE',
-                        metatext: 'Product Type: Market | Country: DE',
-                        items: [],
-                    },
-                ],
-            },
-            {
-                label: 'Wound Care',
-                name: '14',
-                expanded: true,
-                type: 'Market',
-                country: 'DE',
-                metatext: 'Product Type: Market | Country: DE',
-                items: [
-                    {
-                        label: 'Kits and Trays',
-                        name: '15',
-                        expanded: true,
-                        type: 'Market',
-                        country: 'DE',
-                        metatext: 'Product Type: Market | Country: DE',
-                        items: [],
-                    },
-                    {
-                        label: 'Wound Care Accessories',
-                        name: '17',
-                        expanded: true,
-                        type: 'Market',
-                        country: 'DE',
-                        metatext: 'Product Type: Market | Country: DE',
-                        items: [],
-                    },
-                ],
-            },
-        ],
-    },
-];
-
+import getProductCatalogTree from '@salesforce/apex/VDT_ProductAdoptionController.getProductCatalogTree';
 export default class Vdt_productAdoptionTree extends LightningElement {
 
     _productTypes= [
@@ -156,7 +11,16 @@ export default class Vdt_productAdoptionTree extends LightningElement {
         {label: 'Market', value: 'Market', selected: false}
     ];
 
-    items = PRODUCT_DATA;
-    treeData = PRODUCT_DATA;
+    treeData = [];
     
+    connectedCallback() {
+        console.log('connectedCallback')
+        getProductCatalogTree()
+        .then(response => {
+            console.log('response: %O', JSON.stringify(response));
+            this.treeData = response;
+        }).catch(error => {
+            console.log('error on promise: %O', JSON.stringify(error));
+        }).finally();
+    }
 }
