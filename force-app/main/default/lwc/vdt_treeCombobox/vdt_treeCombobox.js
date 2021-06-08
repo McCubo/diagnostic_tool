48ff86c5bcdab7bc764b82c4ef74253194c2ad6c
Miguel Cubias <m.caceres@polsource.com>
2021-06-08 14:21:29 -0600

    adding filtering option feature

--- force-app/main/default/lwc/vdt_treeCombobox/vdt_treeCombobox.js
+++ force-app/main/default/lwc/vdt_treeCombobox/vdt_treeCombobox.js
@@ -6,6 +6,7 @@ export default class Vdt_treeCombobox extends LightningElement {
     @track
     selectedItem;
     items;
+    searchString;
 
     handleOnselect(event) {
         const findNode = (nodeList, name) => nodeList.find((node) => node.name === name) || nodeList.reduce((p, v) => p || (v.items && findNode(v.items, name)), null);
@@ -32,6 +33,15 @@ export default class Vdt_treeCombobox extends LightningElement {
         this.dispatchEvent(selectedEvent);        
     }
 
+    handleSearchTextChanged(event) {
+        if (event.detail) {
+            this.searchString = event.detail.value;
+        } else {
+            this.searchString = null;
+        }
+        
+    }
+
     get buttonLabel() {
         if (this.selectedLabel) {
             return `Currently Selected: ${this.selectedLabel} `;
@@ -41,10 +51,45 @@ export default class Vdt_treeCombobox extends LightningElement {
 
     @api
     get treeData() {
-        return this.items;
+        let filteredItems = [];
+        if (this.searchString) {
+            filteredItems = this.items.filter(item => {
+                return this.showItem(item, this.searchString.toLowerCase());
+            }).map(item => {
+                let _item = JSON.parse(JSON.stringify(item));
+                _item.expanded = this.expandItem(_item, this.searchString.toLowerCase());
+                return _item;
+            });
+        } else {
+            filteredItems = this.items;
+        }
+        return filteredItems;
     }
 
     set treeData(data) {
         this.items = data;
     }
+
+    showItem(item, searchTerm) {
+        let filter = item.label.toLowerCase().includes(searchTerm);
+        let filterChild = [];
+        if (item.items) {
+            filterChild = item.items.filter(childItem => {
+                return this.showItem(childItem, searchTerm);
+            });
+        }
+        return filter || filterChild.length > 0;
+    }
+
+    expandItem(item, searchTerm) {
+        let expand = false;
+        if (item.items) {
+            expand = item.items.reduce((accumulator, currentItem) => {
+                currentItem.expanded = this.expandItem(currentItem, searchTerm);
+                return accumulator || currentItem.label.toLowerCase().includes(searchTerm) || currentItem.expanded;
+            }, false);
+        }
+        return expand;
+    }
+
 }
